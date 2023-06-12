89d7759a0e899d119898fe5f59582353ed11eb65
RotsenCH <117753370+RotsenCH@users.noreply.github.com>
2023-06-12 16:39:06 -0500

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const path = require('path');
+const { engine }  = require('express-handlebars')
+const methodOverride = require('method-override');
+
+// Inicializaciones
+const app = express()
+
+// Configuraciones
+
+app.set('port', process.env.port || 3000)
+
+app.set('views',path.join(__dirname, 'views'))
+
+app.engine('.hbs',engine({
+    defaultLayout:'main',
+    layoutsDir: path.join(app.get('views'),'layouts'),
+    partialsDir: path.join(app.get('views'),'partials'),
+    extname:'.hbs'
+}))
+app.set('view engine','.hbs')
+// Middlewares
+app.use(express.urlencoded({extended:false}))
+app.use(methodOverride('_method'))
+
+// Variables globales
+
+// Rutas 
+app.use(require('./routers/index.routes'))
+app.use(require('./routers/portafolio.routes'))
+
+// Archivos estáticos
+app.use(express.static(path.join(__dirname,'public')))
+
+
+module.exports = app
